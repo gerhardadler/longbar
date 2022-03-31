9b62d09faf697be1cea986a3205636383b633fe9
gerhardadler <gerhardskartveit@gmail.com>
2022-03-31 21:06:47 +0200

    Starting on back end. Using XML to store information on articles.

--- guides/equipment/index.php
+++ guides/equipment/index.php
@@ -1,1 +1,14 @@
-<?php echo file_get_contents('/guides/category-template.html'); ?>
+<?php
+$page = file_get_contents('/home/gerhardadler/www/ct//guides/category-template.html');
+
+$to_be_replaced = array('[category]',
+                        '[description]'
+);
+
+$replacement = array('Equipment',
+                     'Descripton babbbbeeee!'
+);
+
+$page = str_replace($to_be_replaced, $replacement, $page);
+echo $page;
+?>
