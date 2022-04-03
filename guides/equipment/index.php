d3c19d1e60bb2ad303753acf9fca5d48358e62c3
gerhardadler <gerhardskartveit@gmail.com>
2022-04-03 21:04:13 +0200

    Some renaming, fixing on styling, and beginning work on the guide template.

--- guides/equipment/index.php
+++ guides/equipment/index.php
@@ -1,43 +1,43 @@
 <?php
 
-$page = file_get_contents('../categories/category-template.html');
+$page = file_get_contents('../category-template.html');
 
-$articles_xml = simplexml_load_file("../categories/articles.xml");
-$article_template = '
+$guides_xml = simplexml_load_file("../guides.xml");
+$guide_template = '
                     <div class="guide">
-                        <img class="guide-img" src="[article_img]">
+                        <img class="guide-img" src="[guide_img]">
                         <div class="guide-main">
-                            <h2 class="blue">[article_title]</h2>
+                            <h2 class="blue">[guide_title]</h2>
                             <p>
-                                [article_description]
+                                [guide_description]
                             </p>
                             <a href="#" class="guide-button">Read more</a>
                         </div>
                     </div>
 ';
 
-$article_output = '';
+$guide_output = '';
 
-$to_be_replaced = array('[article_title]',
-                        '[article_description]',
-                        '[article_img]'
+$to_be_replaced = array('[guide_title]',
+                        '[guide_description]',
+                        '[guide_img]'
 );
 
-foreach($articles_xml->children() as $article) {
-    if (strpos($article->categories, 'equipment') !== false ) { // If the correct category is used on the article
-        $replacement = array($article->title, $article->description, $article->img);
-        $article_output .= str_replace($to_be_replaced, $replacement, $article_template);
+foreach($guides_xml->children() as $guide) {
+    if (strpos($guide->categories, 'equipment') !== false ) { // If the correct category is used on the guide
+        $replacement = array($guide->title, $guide->description, $guide->img);
+        $guide_output .= str_replace($to_be_replaced, $replacement, $guide_template);
     };
 };
 
 $to_be_replaced = array('[title]',
                         '[description]',
-                        '[articles]'
+                        '[guides]'
 );
 
 $replacement = array('Equipment',
                      'Descripton babbbbeeee!',
-                     $article_output
+                     $guide_output
 );
 
 $page = str_replace($to_be_replaced, $replacement, $page);
