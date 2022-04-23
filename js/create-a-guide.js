b954eb1b4928c73e703e31fbc16270bb4364eb06
gerhardadler <gerhardskartveit@gmail.com>
2022-04-23 17:04:44 +0200

    More work on WYSIWYG

--- js/create-a-guide.js
+++ js/create-a-guide.js
@@ -1,34 +1,46 @@
-var editField = document.getElementsByClassName("ql-editor")[0]
+var editField = document.getElementById("ql-editor")
 var toolbar = document.getElementById("toolbar")
 
-var descriptionEditor = new Quill(".guide-page-description", {
-    modules: {
-        toolbar: "#toolbar-description"
-    },
-    theme: "snow"
-})
-
-var mainEditor = new Quill(".ql-editor", {
+var Editor = new Quill("#ql-editor", {
     modules: {
         toolbar: "#toolbar-main"
     },
     theme: "snow"
 })
 
-//editField.firstChild.setAttribute("tabindex", "0")
+let removeFormatContent = [document.getElementById("guide-page-title"), document.getElementById("guide-page-description")]
 
-//mainEditor.disable()
-//toolbar.innerHTML = toolbar.innerHTML
+removeFormatContent.forEach((item) => {
+    item.addEventListener("keydown", (e) => {
+        if (e.key == "Enter") {
+            e.preventDefault()
+        }
+    })
 
-/*
-editField.firstChild.addEventListener('focus', (event) => {
-    toolbar.innerHTML = toolbarContent
-    mainEditor.enable()
+    // https://stackoverflow.com/a/12028136/12834165
+    item.addEventListener("paste", (e) => {
+        // cancel paste
+        e.preventDefault()
+
+        // get text representation of clipboard
+        var text = (e.originalEvent || e).clipboardData.getData('text/plain')
+
+        // insert text manually
+        document.execCommand("insertHTML", false, text)
+    })
 })
 
-editField.firstChild.addEventListener('blur', (event) => {
-    if (document.activeElement == toolbar) {
-        toolbar.innerHTML = "Select an item to edit"
-        mainEditor.disable()
+document.addEventListener("keydown", (e) => {
+    if (e.key == "Control") {
+        const video = document.querySelectorAll('.ql-video')
+        console.log(video)
+        video.forEach((item, index, object) => {
+            if (!item.parentNode.classList.contains('ql-editor')) {
+                object.slice(index, 1)
+            }
+        })
+        console.log(video)
+        const width = video.offsetWidth
+        video.style.height = width * 2
     }
-})*/
+})
